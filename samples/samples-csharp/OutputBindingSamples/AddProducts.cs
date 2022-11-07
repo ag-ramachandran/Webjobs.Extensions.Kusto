56e9083b5eff990be5134bf3c6d8d4ea753b0c2c
Ramachandran G <ramacg@microsoft.com>
2022-11-07 13:45:41 +0530

    * Fix imports * Remove unused dependencies

--- samples/samples-csharp/OutputBindingSamples/AddProducts.cs
+++ samples/samples-csharp/OutputBindingSamples/AddProducts.cs
@@ -3,7 +3,6 @@
 
 
 using System;
-using System.Globalization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.Kusto.Samples.Common;
